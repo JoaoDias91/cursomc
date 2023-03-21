d06bdeb10bcdf54daa365029e582d20302df329e
Joao Paulo Dias Siqueira <joaodiassiqueira@gmail.com>
2023-03-20 23:05:18 -0300

    Pedido, estado de pagamento e pagamento - repositories e relacionamento

---
+++ src/main/java/com/joao/cursomc/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,11 @@
+package com.joao.cursomc.cursomc.repositories;
+
+import com.joao.cursomc.cursomc.domain.Pagamento;
+import com.joao.cursomc.cursomc.domain.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+
+}
