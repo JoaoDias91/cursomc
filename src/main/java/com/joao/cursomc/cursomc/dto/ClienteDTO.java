a1cf5bc82df42e0aa823701fd9315e521680ed97
joao paulo dias siqueira <joaodiassiqueira@gmail.com>
2023-06-04 18:40:32 -0300

    Validacao customizada. Update de email nao permite utilizar email existente

--- src/main/java/com/joao/cursomc/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/joao/cursomc/cursomc/dto/ClienteDTO.java
@@ -1,12 +1,14 @@
 package com.joao.cursomc.cursomc.dto;
 
 import com.joao.cursomc.cursomc.domain.Cliente;
+import com.joao.cursomc.cursomc.services.validation.ClienteUpdate;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 
     private Integer id;
